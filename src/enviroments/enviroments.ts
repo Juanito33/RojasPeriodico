eb852924e0785c7f1cd262696e14143f7541ba69
juanito33 <holacomoleva695@gmail.com>
2023-04-26 08:55:00 -0500

    primera copia

---
+++ src/enviroments/enviroments.ts
@@ -0,0 +1,14 @@
+export const enviroment = {
+    production:false,
+    firebaseConfig:{
+
+    apiKey: "AIzaSyBQk7NI-xcl866lXnj9ihHTCT6-z4-xo0g",
+    authDomain: "piperiodico-f66b1.firebaseapp.com",
+    projectId: "piperiodico-f66b1",
+    storageBucket: "piperiodico-f66b1.appspot.com",
+    messagingSenderId: "70488002297",
+    appId: "1:70488002297:web:44c8f72535b8926785dc28",
+    measurementId: "G-C64344P5X4"
+    
+    }
+  };
